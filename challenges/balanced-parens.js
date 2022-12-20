8d00d69a02fda24250824916e9ef10340800ffe9
Jack Sonoda <jackksono@gmail.com>
2022-12-20 09:55:41 -0800

    WIP Balances Parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,38 @@
  */
 
 const balancedParens = input => {
+    let arr = input.split(``)
+    let newArr = [];
+    console.log(arr)
+   
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
+        newArr.push(arr[i])}
+        console.log(newArr)
+        if (newArr.length === 0) return false;
 
+        if (arr.pop() === ')') {
+            if (newArr[0] === '(' || `{` || `[`) return true
+        }
+
+        if (arr.pop() === ']') {
+            if (newArr[0] === '(' || `{` || `[`) return true
+        }
+
+        if (arr.pop() === '}') {
+            if (newArr[0] === '(' || `{` || `[`) return true
+        }
+        else return false
+    }
 };
 
 module.exports = { balancedParens} ;
+
+console.log(balancedParens('(')) // false
+console.log(balancedParens('()')); // true
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true)
+
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); //false
