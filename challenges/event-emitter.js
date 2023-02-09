17082273b3f6761f5758a42bcf96fea3a624947f
Jack Sonoda <jackksono@gmail.com>
2023-02-09 09:49:32 -0800

    WIP HH

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -28,14 +28,26 @@ Caveats:
 
 function EventEmitter() {
 
+  this.listeners = {}
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
 
+  if (!this.listeners[funcName]) {
+    this.listeners[funcName] = [];
+  }
+  this.listeners[funcName].push(func)
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
 
+  if (this.listeners[funcName]) {
+    this.listeners[funcName].forEach((func) => func(...args))
+  }
 };
 
+const instance = new EventEmitter();
+
+let counter1 = 0;
+let counter2 = 0;
 module.exports = EventEmitter;
