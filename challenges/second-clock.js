f621b68a541e71de254e750547ccf8670514522d
Jack Sonoda <jackksono@gmail.com>
2023-03-01 09:47:35 -0800

    Working Second-Clock

--- challenges/second-clock.js
+++ challenges/second-clock.js
@@ -31,11 +31,17 @@ Hint: look up setInterval and clearInterval.
 class SecondClock {
   constructor(cb) {
     this.cb = cb;
+    this.intervalID;
+    this.second = 0;
   }
-
-  start() {}
-
-  stop() {}
+  start() {
+    this.intervalID = setInterval(() => this.cb( this.second++ % 60 + 1 ), 1000);
+  }
+  stop() {
+    this.second = 0;
+    clearInterval(this.intervalID);
+  }
 }
+
 
 module.exports = { SecondClock };
