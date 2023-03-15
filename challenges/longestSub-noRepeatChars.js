c78ceaf9ac0d4c6cf383e5bcc554bc5397b5d742
Jack Sonoda <jackksono@gmail.com>
2023-03-14 21:41:53 -0700

    Working on longestSub-noRepeat

--- challenges/longestSub-noRepeatChars.js
+++ challenges/longestSub-noRepeatChars.js
@@ -1,17 +1,21 @@
 //Given a string s, find the length of the longest substring without repeating characters.
 
 var lengthOfLongestSubstring = function(s) {
-    if (s === null) return 1
+    if (s.charCodeAt(0) <= 32) return 1
     let string = '';
     let tempStr = '';
     for (let i = 0; i < s.length; i++) {
       	if (tempStr.length > string.length) string = tempStr
         else if (tempStr.includes(s[i])) string = tempStr
         else tempStr += s[i]
+        // console.log(string)
+        // console.log(tempStr)
     }
+    //if (string.length > tempStr.length) return string.length
     return string.length
 };
 
-console.log('abcabcabc') //3
-console.log('bbbb') //1
-console.log('pwwkew') //3
+console.log(lengthOfLongestSubstring('abcabcabc')) //3
+console.log(lengthOfLongestSubstring('bbbb')) //1
+console.log(lengthOfLongestSubstring('pwwkew')) //3
+console.log(lengthOfLongestSubstring('au'))
