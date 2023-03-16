7567daec987ac014b977d0a11c8519557b919674
Jack Sonoda <jackksono@gmail.com>
2023-03-15 20:45:10 -0700

    Finished longestSub

--- challenges/longestSub-noRepeatChars.js
+++ challenges/longestSub-noRepeatChars.js
@@ -3,18 +3,24 @@
 var lengthOfLongestSubstring = function(s) {
     if (s.charCodeAt(0) <= 32) return 1
     let string = '';
+    let tempStr = '';
     for (let i = 0; i < s.length; i++) {
       	// if (tempStr.length > string.length) string = tempStr
-        if (string.includes(s[i])) return string.length
-        else string += s[i]
+        if (tempStr.includes(s[i]) && tempStr.length > string.length) {
+            string = tempStr;
+            tempStr = ''
+        } 
+        else if (tempStr.includes(s[i])) return tempStr.length
+        else tempStr += s[i]
         // console.log(string)
         // console.log(tempStr)
     }
     //if (string.length > tempStr.length) return string.length
-    return string.length
+    if (string.length > tempStr.length) return string.length
+    else return tempStr.length
 };
 
-console.log(lengthOfLongestSubstring('abcabcabc')) //3
+console.log(lengthOfLongestSubstring('abcabcbb')) //3
 console.log(lengthOfLongestSubstring('bbbb')) //1
 console.log(lengthOfLongestSubstring('pwwkew')) //3
 console.log(lengthOfLongestSubstring('au'))
