715b739df526d4b41bb3c6cfed24e993934afb7f
Jack Sonoda <jackksono@gmail.com>
2023-03-16 18:41:33 -0700

    finished median of 2 arrays

--- challenges/LeetCode/Median of 2 arrays.js
+++ challenges/LeetCode/Median of 2 arrays.js
@@ -1,12 +1,18 @@
 var findMedianSortedArrays = function(nums1, nums2) {
-    let newArray = nums1.concat(nums2);
-    let total = 0;
-    for (let i = 0; i < newArray.length; i++) {
-        total = ( newArray.length)/2
-    }
-    console.log(total)
-    return total
-};
+    // Combine the two arrays into a single sorted array
+  const mergedArray = nums1.concat(nums2).sort((a, b) => a - b);
+  const totalLength = mergedArray.length;
+  const mid = Math.floor(totalLength / 2);
+
+  // Check if the length of the merged array is even or odd
+  if (totalLength % 2 === 0) {
+    // If the length is even, return the average of the two middle elements
+    return (mergedArray[mid - 1] + mergedArray[mid]) / 2;
+  } else {
+    // If the length is odd, return the middle element
+    return mergedArray[mid];
+  }
+}
 
 console.log(findMedianSortedArrays([1,3], [2]))
 console.log(findMedianSortedArrays([1,3], [2,7]))
