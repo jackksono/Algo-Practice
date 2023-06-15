f850f4e0e4a6c902d5ccda012a8af47c7767d93c
Jack Sonoda <jackksono@gmail.com>
2023-06-15 12:49:53 -0700

    Completed SearchInsertPosition

---
+++ challenges/LeetCode/SearchInsertPosition.js
@@ -0,0 +1,15 @@
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.*/
+
+var searchInsert = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) return i;
+        else if (target <= nums[i]) return i
+    }
+   return nums.length 
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5))
+console.log(searchInsert([1, 3, 5, 6], 2))
+console.log(searchInsert([1, 3, 5, 6], 7))
