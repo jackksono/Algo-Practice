e0cbb0226f0cb529a16d9156a3db9a7407b27f26
Jack Sonoda <jackksono@gmail.com>
2023-06-18 12:14:48 -0700

    Finished

--- challenges/LeetCode/SearchInsertPosition.js
+++ challenges/LeetCode/SearchInsertPosition.js
@@ -12,4 +12,4 @@ var searchInsert = function(nums, target) {
 
 console.log(searchInsert([1, 3, 5, 6], 5))
 console.log(searchInsert([1, 3, 5, 6], 2))
-console.log(searchInsert([1, 3, 5, 6], 7))
+console.log(searchInsert([1, 3, 5, 6], 7)) 
