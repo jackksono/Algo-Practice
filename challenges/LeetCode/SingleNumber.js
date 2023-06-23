5907fe566dd1e5d5c028aaf4b9844e69af269d02
Jack Sonoda <jackksono@gmail.com>
2023-06-22 23:49:25 -0700

    2 ways to solve

--- challenges/LeetCode/SingleNumber.js
+++ challenges/LeetCode/SingleNumber.js
@@ -4,13 +4,29 @@ You must implement a solution with a linear runtime complexity and use only cons
 
  */
 
-var singleNumber = function(nums) {
-    let output = 0
+function singleNumber(nums) {
+    const numSet = new Set();
+  
     for (let i = 0; i < nums.length; i++) {
-        if (nums.includes(nums[i])) continue;
-        else output = nums[i]
+      if (numSet.has(nums[i])) {
+        numSet.delete(nums[i]);
+      } else {
+        numSet.add(nums[i]);
+      }
     }
-    return output
-};
+  
+    return Array.from(numSet)[0];
+  }
 
-console.log(singleNumber([2,2,1]))
+  function singleNumber(nums) {
+    let result = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+      result ^= nums[i];
+    }
+    
+    return result;
+  }
+  
+
+console.log(singleNumber([2,2,1,1, 3]))
